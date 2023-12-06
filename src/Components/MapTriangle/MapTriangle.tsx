32a75249ca4b088b391fc93eef37063373c8a00d
ptaku35 <phoenixtaku9@gmail.com>
2023-12-07 00:04:44 +0700

    updated map with markers

---
+++ src/Components/MapTriangle/MapTriangle.tsx
@@ -0,0 +1,81 @@
+// MapTriangle.tsx
+import { Box, Tooltip, keyframes } from "@chakra-ui/react";
+import React from "react";
+
+// Define the props for the MapTriangle component
+interface MapTriangleProps {
+  top: string; // Position from top
+  left: string; // Position from left
+  label: string; // Tooltip text
+  onClick: () => void; // Click event handler
+}
+
+// Define the keyframes for the dancing animation
+const dancingAnimation = keyframes`
+  0%, 100% { transform: translateY(-5%); }
+  50% { transform: translateY(5%); }
+`;
+
+// The MapTriangle component
+const MapTriangle: React.FC<MapTriangleProps> = ({
+  top,
+  left,
+  label,
+  onClick,
+}) => {
+  // Update this clipPath to create the desired hexagonal shape
+  const triangleClipPath = `
+  polygon(
+    50% 30%,       /* Top point */
+    58% 30%,       /* Top-right curve start */
+    80% 30%,      /* Top-right curve end */
+    100% 50%,     /* Right middle point */
+    80% 70%,      /* Bottom-right curve end */
+    58% 92%,      /* Bottom-right curve start */
+    50% 100%,     /* Bottom point */
+    42% 92%,      /* Bottom-left curve start */
+    20% 70%,      /* Bottom-left curve end */
+    0% 50%,       /* Left middle point */
+    20% 30%,      /* Top-left curve end */
+    42% 30%        /* Top-left curve start */
+  )
+`;
+
+  return (
+    <Box
+      position="absolute"
+      top={top}
+      left={left}
+      transform="translate(-50%, -50%)"
+    >
+      <Tooltip
+        label={label}
+        hasArrow={false}
+        placement="bottom"
+        bg="yellow.500"
+        color="white"
+        py={3}
+        px={5}
+        mt="-70px"
+        borderRadius="md"
+        boxShadow="none"
+      >
+        <Box
+          as="button"
+          w="40px"
+          h="60px" // Adjust height to match the hexagonal aspect ratio
+          bg="yellow.500"
+          borderRadius="4px" // Adjust this for slight rounding of the corners
+          clipPath={triangleClipPath} // Apply the hexagonal clip path
+          animation={`${dancingAnimation} 3s ease-in-out infinite`}
+          _hover={{
+            animationPlayState: "paused",
+          }}
+          onClick={onClick}
+        />
+      </Tooltip>
+    </Box>
+  );
+};
+
+export default MapTriangle;
--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -1,37 +1,88 @@
-//import styles from "../styles/Home.module.css";
+// Import necessary components from Chakra UI
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Image,
+  useBoolean,
+} from "@chakra-ui/react";
 import Layout from "../../Components/Layout/Layout";
 import Head from "next/head";
-import { Box, Center, Flex, Heading, Image } from "@chakra-ui/react";
 import Container from "Components/Container/Container";
+import MapTriangle from "Components/MapTriangle/MapTriangle";
 
 export default function Play() {
+  // Function to handle the click event
+  const handleClick = () => {
+    // Implement what should happen when a triangle is clicked
+    console.log("Triangle clicked!");
+  };
+
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>Buzzkill Play Game</title>
-          {/* <meta name="description" content="noindex,nofollow" /> */}
-        </Head>
-        <Container fullWidth={true}>
-          <Box position="relative" width="full" height="90vh" overflow="hidden">
-            {/* Use the native video tag here */}
-            <video
-              width="100%" // Make the video responsive by setting width to 100%
-              height="auto" // Set height to auto to maintain aspect ratio
-              autoPlay // Add autoPlay to play the video as soon as it's ready
-              loop // Add loop to continuously loop the video
-              muted // Mute the video to play it automatically in most browsers
-              style={{
-                borderRadius: "0px", // Apply border-radius if needed
-                objectFit: "cover", // Cover the box area just like objectFit for images
-              }}
-            >
-              <source src="/volcano-smoke-map.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </Box>
-        </Container>
-      </Layout>
-    </>
+    <Layout>
+      <Head>
+        <title>Buzzkill Play Game</title>
+      </Head>
+      <Container fullWidth={true}>
+        <Box position="relative" width="full" height="90vh" overflow="hidden">
+          <video
+            width="100%"
+            height="auto"
+            autoPlay
+            loop
+            muted
+            style={{
+              objectFit: "cover",
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <source src="/volcano-smoke-map.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          {/* Overlay triangles */}
+          <MapTriangle
+            top="80%" // Adjust as needed
+            left="75%" // Adjust as needed
+            label="Sunlit Sands Hive"
+            onClick={handleClick}
+          />
+          {"Sunlit Sands Hive"}
+
+          <MapTriangle
+            top="15%" // Adjust as needed
+            left="85%" // Adjust as needed
+            label="Emberglow Caldera Hive"
+            onClick={handleClick}
+          />
+          {"Emberglow Caldera Hive"}
+
+          <MapTriangle
+            top="70%" // Adjust as needed
+            left="25%" // Adjust as needed
+            label="Verdant Canopy Hive"
+            onClick={handleClick}
+          />
+          {"Verdant Canopy Hive"}
+
+          <MapTriangle
+            top="80%" // Adjust as needed
+            left="10%" // Adjust as needed
+            label="Azure Marina Hive"
+            onClick={handleClick}
+          />
+          {"Azure Marina Hive"} 
+
+          <MapTriangle
+            top="35%" // Adjust as needed
+            left="65%" // Adjust as needed
+            label="Frostwing Glacier Hive"
+            onClick={handleClick}
+          />
+          {"Frostwing Glacier Hive"}  
+        </Box>
+      </Container>
+    </Layout>
   );
 }
