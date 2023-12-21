88b0beb12867ef8026d1a50a41e6a4e73f7d3421
ptaku35 <phoenixtaku9@gmail.com>
2023-12-21 16:00:34 +0700

    updated the standard Layout so if the content is smaller than 100vh the footer still sits at hte bottom

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { Box, Flex } from "@chakra-ui/react";
 import Head from "next/head";
-
 import Footer from "../Navigation/Footer/Footer";
 import Header from "../Navigation/Header/Header";
 import React from "react";
@@ -9,6 +8,10 @@ import useMeasureHeight from "hooks/useMeasureHeight";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { ref, headerHeight } = useMeasureHeight();
+
+  // Calculate the minimum content height
+  const minContentHeight = `calc(100vh - ${headerHeight}px - 10vh)`; // 10vh or any desired minimum height
+
   return (
     <>
       <Head>
@@ -16,25 +19,27 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Buzzkill</title>
       </Head>
-      {/* this is the whole screen background*/}
       <div className={styles.flex_background}>
         <div ref={ref}>
           <Header />
         </div>
-
         <Flex
           direction="column"
           minH={`calc(100vh - ${headerHeight}px)`}
           textAlign="center"
+          style={{ display: "flex", flexDirection: "column" }}
         >
           <Box
+            flex="1"
+            minH={minContentHeight} // Set the minimum height of the content
             bg="transparent"
             backgroundSize="cover"
             backgroundAttachment="fixed"
+            display="flex" // Make the content a flex container
+            flexDirection="column" // Set the direction to column
           >
             {children}
           </Box>
-
           <Footer />
         </Flex>
       </div>
