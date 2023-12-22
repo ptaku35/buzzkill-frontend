086cff079c063f41e35b924241764e84dfb24c33
ptaku35 <phoenixtaku9@gmail.com>
2023-12-22 14:42:48 +0800

    fixed build errors on dynami page routing tempaltes

---
+++ src/pages/play/[regionID]/[hiveID].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
+import ResourcePin from "Components/ResourcePin/ResourcePin";
+import MapTriangle from "Components/MapTriangle/MapTriangle";
+
+const HivePage = () => {
+  const router = useRouter();
+
+  return <Heading>Heading</Heading>;
+};
+export default HivePage;
